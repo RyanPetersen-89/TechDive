4359a3e737216c75f23022d93287643ebdbc66de
Ryan Petersen <rp26898@gmail.com>
2024-07-15 16:41:14 -0700

    added post.js and comment.js files to ../models/ to define what a post and a comment are.

---
+++ models/post.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {}
+
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      max: 20,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      max: 500,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    timestamps: true,
+    modelName: 'post',
+    hooks: {
+      beforeCreate: async (post) => {
+        post.post_date = new Date();
+      },
+    },
+  }
+);
+
+module.exports = Post;
