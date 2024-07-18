d1ec8250a6b3ab8bc63f0ad081cad504578a6b51
Ryan Petersen <rp26898@gmail.com>
2024-07-17 18:18:20 -0700

    making changes so that I can push to Render

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,11 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
-const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(
-      process.env.DB_NAME,
-      process.env.DB_USER,
-      process.env.DB_PASSWORD,
-      {
-        host: 'localhost',
-        dialect: 'postgres',
-      }
-    );
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  logging: false,
+  dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
+});
 
 module.exports = sequelize;
